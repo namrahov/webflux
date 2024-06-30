d87898b7f0f3007864fec8a25cc3d5061b4ba2b7
namrahov <amrahovnurlan@gmail.com>
2024-06-30 20:44:58 +0400

    1

--- src/main/java/com/ahmed/dto/ResponseDto.java
+++ src/main/java/com/ahmed/dto/ResponseDto.java
@@ -1,10 +1,12 @@
 package com.ahmed.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 public class ResponseDto {
     private Date data = new Date();
     private int output;
